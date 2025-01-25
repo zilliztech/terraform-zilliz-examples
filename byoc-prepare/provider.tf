484aa1b43e40be8fd7d1d58665f0087c0b1e64fd
Yellow Shine <sammy.huang@zilliz.com>
2025-01-25 18:14:58 +0800

    provider
    Signed-off-by: Yellow Shine <sammy.huang@zilliz.com>

---
+++ byoc-prepare/provider.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = ">=1.6.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.82.1"
+    }
+    zillizcloud = {
+      source  = "zilliztech/zillizcloud"
+    }
+  }
+}
+
+provider "aws" {
+  # profile = "byoc"
+  region  = var.aws_region
+}
+
+provider "zillizcloud" {
+    api_key   = "11a7e4709596d0970d36cdb73387205cdf9f151c6ca247208ae0c57dc2173a58abf8e60e3068f5b5bc5e119958be5bc5f30bce2f"
+    byoc_mode = true
+}

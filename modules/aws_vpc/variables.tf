c39c3639ce2e77c5d8ad97901c578a4a2f54f76c
Yellow Shine <sammy.huang@zilliz.com>
2025-02-14 16:10:40 +0800

    feat: support private link
    Signed-off-by: Yellow Shine <sammy.huang@zilliz.com>

--- modules/aws_vpc/variables.tf
+++ modules/aws_vpc/variables.tf
@@ -24,3 +24,10 @@ variable "name" {
     error_message = "variable name cannot be empty."
   }
 }
+
+variable "enable_private_link" {
+  description = "Enable private link for the byoc project"
+  type        = bool
+  default     = false
+}
+

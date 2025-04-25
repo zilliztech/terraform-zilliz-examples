5aee8e8673706ea093cdfbd20f3d5e303711d94a
AlintaLu <yiqing.lu@zilliz.com>
2025-04-25 15:26:05 +0800

    feat: add gcp private link module

---
+++ modules/gcp/private-link/output.tf
@@ -0,0 +1,7 @@
+output "byoc_endpoint_id" {
+    value = google_compute_forwarding_rule.byoc_endpoint.id
+}
+
+output "byoc_endpoint_private_zone_id" {
+    value = google_dns_managed_zone.byoc_endpoint_private_zone.id
+}

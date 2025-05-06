7d040b9d18787210a48ba5243072939369b8dfcb
Yellow Shine <sammy.huang@zilliz.com>
2025-05-06 19:50:06 +0800

    create a identity pool
    Signed-off-by: Yellow Shine <sammy.huang@zilliz.com>

--- modules/gcp/iam/iam_storage_sa.tf
+++ modules/gcp/iam/iam_storage_sa.tf
@@ -29,8 +29,15 @@ resource "google_project_iam_member" "storage-bucket-viewer-binding" {
   }
 }
 
+resource "google_iam_workload_identity_pool" "gke_pool" {
+  workload_identity_pool_id = var.gcp_project_id
+  display_name              = "GKE Workload Identity Pool"
+  description              = "Identity pool for GKE workload identity"
+  project                  = var.gcp_project_id
+}
+
 resource "google_service_account_iam_member" "cluster-workload-identity" {
   service_account_id = google_service_account.storage-sa.name
   role    = "roles/iam.workloadIdentityUser"
-  member  = "principalSet://iam.googleapis.com/projects/${data.google_project.project.number}/locations/global/workloadIdentityPools/${var.gcp_project_id}.svc.id.goog/kubernetes.cluster/https://container.googleapis.com/v1/projects/${var.gcp_project_id}/locations/${var.gcp_region}/clusters/${var.gke_cluster_name}"
+  member  = "principalSet://iam.googleapis.com/projects/${data.google_project.project.number}/locations/global/workloadIdentityPools/${google_iam_workload_identity_pool.gke_pool.workload_identity_pool_id}/kubernetes.cluster/https://container.googleapis.com/v1/projects/${var.gcp_project_id}/locations/${var.gcp_region}/clusters/${var.gke_cluster_name}"
 }

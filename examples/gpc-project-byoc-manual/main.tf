82d54500ddfa542c7a0a28cdcf3e9d5995450329
Yellow Shine <sammy.huang@zilliz.com>
2025-05-21 10:13:03 +0800

    fix
    Signed-off-by: Yellow Shine <sammy.huang@zilliz.com>

--- examples/gpc-project-byoc-manual/main.tf
+++ examples/gpc-project-byoc-manual/main.tf
@@ -1,4 +1,17 @@
 # VPC Module
+//random id
+resource "random_id" "short_uuid" {
+  byte_length = 3 # 3 bytes = 6 characters when base64 encoded
+}
+locals {
+  bucket_name                              = var.customer_bucket_name != "" ? var.customer_bucket_name : "${var.customer_vpc_name}-bucket-${random_id.short_uuid.hex}"
+  prefix_name                              = var.customer_vpc_name
+  customer_gke_cluster_name                = var.customer_gke_cluster_name != "" ? var.customer_gke_cluster_name : "${var.customer_vpc_name}-gke"
+  customer_storage_service_account_name    = var.customer_storage_service_account_name != "" ? var.customer_storage_service_account_name : "${var.customer_vpc_name}-storage-sa"
+  customer_management_service_account_name = var.customer_management_service_account_name != "" ? var.customer_management_service_account_name : "${var.customer_vpc_name}-management-sa"
+  customer_gke_node_service_account_name   = var.customer_gke_node_service_account_name != "" ? var.customer_gke_node_service_account_name : "${var.customer_vpc_name}-gke-node-sa"
+}
+
 module "vpc" {
   source = "../../modules/gcp/vpc"
 
@@ -63,3 +76,22 @@ module "iam" {
   gke_node_service_account_name   = local.customer_gke_node_service_account_name
   delegate_from                   = var.zilliz_service_account
 }
+
+
+output "network_settings" {
+  value = module.vpc
+}
+
+output "credential_settings" {
+  value = module.iam
+}
+
+output "private_service_connection" {
+  value = var.enable_private_link ? module.private_link[0].byoc_endpoint_ip : null
+}
+
+output "bucket_name" {
+  value = local.bucket_name
+}
+
+

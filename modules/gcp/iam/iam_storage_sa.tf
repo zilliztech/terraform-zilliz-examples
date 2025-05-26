11902f437cd01eb9cbc89590923d4d175f955f5a
Yellow Shine <sammy.huang@zilliz.com>
2025-05-26 20:01:44 +0800

    patch: can not use iam workload identity pool
    Signed-off-by: Yellow Shine <sammy.huang@zilliz.com>

--- modules/gcp/iam/iam_storage_sa.tf
+++ modules/gcp/iam/iam_storage_sa.tf
@@ -28,16 +28,3 @@ resource "google_project_iam_member" "storage-bucket-viewer-binding" {
     expression  = "resource.name.startsWith(\"projects/_/buckets/${var.storage_bucket_name}\")"
   }
 }
-
-resource "google_iam_workload_identity_pool" "gke_pool" {
-  workload_identity_pool_id = "${var.gcp_project_id}-${random_id.short_uuid.hex}"
-  display_name              = "GKE Workload Identity Pool"
-  description              = "Identity pool for GKE workload identity"
-  project                  = var.gcp_project_id
-}
-
-resource "google_service_account_iam_member" "cluster-workload-identity" {
-  service_account_id = google_service_account.storage-sa.name
-  role    = "roles/iam.workloadIdentityUser"
-  member  = "principalSet://iam.googleapis.com/projects/${data.google_project.project.number}/locations/global/workloadIdentityPools/${google_iam_workload_identity_pool.gke_pool.workload_identity_pool_id}/kubernetes.cluster/https://container.googleapis.com/v1/projects/${var.gcp_project_id}/locations/${var.gcp_region}/clusters/${var.gke_cluster_name}"
-}

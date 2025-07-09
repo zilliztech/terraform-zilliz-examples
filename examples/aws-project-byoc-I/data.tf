18e5967e7ca6c5fd7b1f26cb0f88796042df7d64
Sammy Huang <sammy.huang@zilliz.com>
2025-07-09 11:18:29 +0800

    fix: a bug in byoc-i
    Signed-off-by: Sammy Huang <sammy.huang@zilliz.com>

--- examples/aws-project-byoc-I/data.tf
+++ examples/aws-project-byoc-I/data.tf
@@ -6,10 +6,10 @@ locals {
   short_project_id = substr(data.zillizcloud_byoc_i_project_settings.this.id, 0, 10)
   prefix_name = "zilliz-${local.short_project_id}-${random_id.short_uuid.hex}"
   dataplane_id = data.zillizcloud_byoc_i_project_settings.this.data_plane_id
-  vpc_id = local.is_existing_vpc ? var.customer_vpc_id :module.my_vpc[0].vpc_id
-  security_group_id = local.is_existing_vpc ? var.customer_security_group_id : module.my_vpc[0].security_group_id
-  subnet_ids =  local.is_existing_vpc ? var.customer_private_subnet_ids : module.my_vpc[0].private_subnets
-  eks_control_plane_subnet_ids = local.is_existing_vpc ? var.customer_eks_control_plane_private_subnet_ids : module.my_vpc[0].private_subnets
+  vpc_id = local.is_existing_vpc ? var.customer_vpc_id :module.vpc[0].vpc_id
+  security_group_id = local.is_existing_vpc ? var.customer_security_group_id : module.vpc[0].security_group_id
+  subnet_ids =  local.is_existing_vpc ? var.customer_private_subnet_ids : module.vpc[0].private_subnets
+  eks_control_plane_subnet_ids = local.is_existing_vpc ? var.customer_eks_control_plane_private_subnet_ids : module.vpc[0].private_subnets
   region = replace(data.zillizcloud_byoc_i_project_settings.this.region, "aws-", "")
   enable_private_link = var.enable_private_link != null ? var.enable_private_link : data.zillizcloud_byoc_i_project_settings.this.private_link_enabled
   external_id = data.zillizcloud_external_id.current.id
@@ -21,15 +21,15 @@ locals {
   k8s_node_groups = data.zillizcloud_byoc_i_project_settings.this.node_quotas
   project_id = data.zillizcloud_byoc_i_project_settings.this.project_id
   data_plane_id = data.zillizcloud_byoc_i_project_settings.this.data_plane_id
-  s3_bucket_id = module.my_s3.s3_bucket_id
-  eks_role = module.my_eks.eks_role
-  maintenance_role = module.my_eks.maintenance_role
-  eks_addon_role = module.my_eks.eks_addon_role
-  storage_role = module.my_eks.storage_role
-  byoc_endpoint = local.enable_private_link ? module.my_private_link[0].endpoint_id : null
+  s3_bucket_id = module.s3.s3_bucket_id
+  eks_role = module.eks.eks_role
+  maintenance_role = module.eks.maintenance_role
+  eks_addon_role = module.eks.eks_addon_role
+  storage_role = module.eks.storage_role
+  byoc_endpoint = local.enable_private_link ? module.private_link[0].endpoint_id : null
 
   ext_config = {
-    eks_cluster_name = module.my_eks.eks_cluster_name
+    eks_cluster_name = module.eks.eks_cluster_name
     ecr = var.customer_ecr
   }
 }

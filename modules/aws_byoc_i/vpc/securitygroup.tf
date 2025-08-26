c07bb84f3ad99429e2a42684ee7aa36676ccea9c
Sammy Huang <sammy.huang@zilliz.com>
2025-08-26 15:23:25 +0800

    fix: sg issue
    for best practice, use aws_vpc_security_group_egress_rule and
    aws_vpc_security_group_ingress_rule instead of in-line ingress egress
    rules
    
    Signed-off-by: Sammy Huang <sammy.huang@zilliz.com>

--- modules/aws_byoc_i/vpc/securitygroup.tf
+++ modules/aws_byoc_i/vpc/securitygroup.tf
@@ -3,58 +3,83 @@ resource "aws_security_group" "zilliz_byoc_security_group" {
   vpc_id      = module.vpc.vpc_id
   description = "Default security group of the VPC"
 
-  dynamic "ingress" {
-    for_each = local.sg_ingress_protocol
-    content {
-      from_port   = 0
-      to_port     = 65535
-      protocol    = ingress.value
-      self = true
-    }
-  }
-
-  dynamic "ingress" {
-    for_each = local.sg_ingress_protocol
-    content {
-      from_port   = 0
-      to_port     = 65535
-      protocol    = ingress.value
-      cidr_blocks = [local.vpc_cidr]
-    }
-  }
-
-  dynamic "egress" {
-    for_each = local.sg_egress_protocol
-    content {
-      from_port = 0
-      to_port   = 65535
-      protocol  = egress.value
-      self      = true
-    }
-  }
-
-  dynamic "egress" {
-    for_each = local.sg_egress_protocol
-    content {
-      from_port = 0
-      to_port   = 65535
-      protocol  = egress.value
-      cidr_blocks = [local.vpc_cidr]
-    }
-  }
-
-  dynamic "egress" {
-    for_each = local.sg_egress_ports
-    content {
-      from_port   = egress.value
-      to_port     = egress.value
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-  }
-
   tags = merge({
     Name = "${local.prefix_name}-sg"
     Vendor = "zilliz-byoc"
   }, var.custom_tags)
+}
+
+# Ingress rules - self-referencing for protocols
+resource "aws_vpc_security_group_ingress_rule" "self_ingress" {
+  for_each          = toset(local.sg_ingress_protocol)
+  security_group_id = aws_security_group.zilliz_byoc_security_group.id
+  
+  from_port                    = 0
+  to_port                      = 65535
+  ip_protocol                  = each.value
+  referenced_security_group_id = aws_security_group.zilliz_byoc_security_group.id
+  
+  tags = {
+    Name = "${local.prefix_name}-sg-ingress-self-${each.value}"
+  }
+}
+
+# Ingress rules - VPC CIDR for protocols
+resource "aws_vpc_security_group_ingress_rule" "vpc_ingress" {
+  for_each          = toset(local.sg_ingress_protocol)
+  security_group_id = aws_security_group.zilliz_byoc_security_group.id
+  
+  from_port   = 0
+  to_port     = 65535
+  ip_protocol = each.value
+  cidr_ipv4   = local.vpc_cidr
+  
+  tags = {
+    Name = "${local.prefix_name}-sg-ingress-vpc-${each.value}"
+  }
+}
+
+# Egress rules - self-referencing for protocols
+resource "aws_vpc_security_group_egress_rule" "self_egress" {
+  for_each          = toset(local.sg_egress_protocol)
+  security_group_id = aws_security_group.zilliz_byoc_security_group.id
+  
+  from_port                    = 0
+  to_port                      = 65535
+  ip_protocol                  = each.value
+  referenced_security_group_id = aws_security_group.zilliz_byoc_security_group.id
+  
+  tags = {
+    Name = "${local.prefix_name}-sg-egress-self-${each.value}"
+  }
+}
+
+# Egress rules - VPC CIDR for protocols
+resource "aws_vpc_security_group_egress_rule" "vpc_egress" {
+  for_each          = toset(local.sg_egress_protocol)
+  security_group_id = aws_security_group.zilliz_byoc_security_group.id
+  
+  from_port   = 0
+  to_port     = 65535
+  ip_protocol = each.value
+  cidr_ipv4   = local.vpc_cidr
+  
+  tags = {
+    Name = "${local.prefix_name}-sg-egress-vpc-${each.value}"
+  }
+}
+
+# Egress rules - external access for specific ports
+resource "aws_vpc_security_group_egress_rule" "external_egress" {
+  for_each          = toset([for port in local.sg_egress_ports : tostring(port)])
+  security_group_id = aws_security_group.zilliz_byoc_security_group.id
+  
+  from_port   = tonumber(each.value)
+  to_port     = tonumber(each.value)
+  ip_protocol = "tcp"
+  cidr_ipv4   = "0.0.0.0/0"
+  
+  tags = {
+    Name = "${local.prefix_name}-sg-egress-external-${each.value}"
+  }
 }

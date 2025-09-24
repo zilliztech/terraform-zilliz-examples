a26570e18ff045662c8ad09b413b7d3ab63541c1
Sammy Huang <sammy.huang@zilliz.com>
2025-09-24 14:58:54 +0800

    use sg when it is created
    Signed-off-by: Sammy Huang <sammy.huang@zilliz.com>

--- modules/aws_byoc_i/privatelink/data.tf
+++ modules/aws_byoc_i/privatelink/data.tf
@@ -1,5 +1,6 @@
 data "aws_caller_identity" "current" {}
 
 locals {
-  config = yamldecode(file("${path.module}/../../conf.yaml"))
+  config             = yamldecode(file("${path.module}/../../conf.yaml"))
+  security_group_ids = var.create_security_group ? [aws_security_group.byoc_endpoint_sg[0].id] : var.security_group_ids
 }
